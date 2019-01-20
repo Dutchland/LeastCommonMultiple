0c91b126aaef24ddc4cced6da8298f835dd06014
Unknown <marcelotto89@gmail.com>
2019-01-20 12:52:52 +0100

    Implementation of algorithm

--- src/main/java/nl/dutchland/leastcommonmultiple/LeastCommonMultiple.java
+++ src/main/java/nl/dutchland/leastcommonmultiple/LeastCommonMultiple.java
@@ -1,6 +1,10 @@
 package nl.dutchland.leastcommonmultiple;
 
 import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LeastCommonMultiple {
     private final int from;
@@ -12,6 +16,25 @@ public class LeastCommonMultiple {
     }
 
     public BigInteger value() {
-        return BigInteger.ONE;
+        Queue<BigInteger> range = createRange(from, until);
+        return leastCommonMultiple(range);
+    }
+
+    private static Queue<BigInteger> createRange(int start, int end) {
+        return IntStream.rangeClosed(start, end)
+                .mapToObj(BigInteger::valueOf)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    private static BigInteger leastCommonMultiple(Queue<BigInteger> queue) {
+        if (queue.isEmpty()) {
+            return BigInteger.ONE;
+        } else {
+            BigInteger currentValue = queue.poll();
+            BigInteger leastCommonMultiple = leastCommonMultiple(queue);
+
+            return leastCommonMultiple.multiply(currentValue)
+                    .divide(leastCommonMultiple.gcd(currentValue));
+        }
     }
 }
